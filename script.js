ada1c7d2304d9fa3a011dd666b8baef4a10edc29
XabibullayevAbror <abrorboiiixabibullayev@gmail.com>
2024-11-03 21:16:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let a = prompt('ismingizni kiriting')
+let b = prompt('idingizni kiriting')
+let c = prompt('parolingiz kiriting')
+
+
+
+let data = {
+    name: "Abror",
+    id: 1401,
+    parol: 12345,
+
+    name: "Ayub",
+    id: 1402,
+    parol: 900970,
+
+    name: "Saidalo",
+    id: 1403,
+    parol: 00110,
+}
+
+console.log(data);
